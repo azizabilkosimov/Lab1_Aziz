c360ff6b99b2da6041d614caba55464b94be01d7
azizabilkosimov <aza6412@psu.edu>
2026-01-20 11:44:34 -0500

    Added objects

---
+++ SCR/Main.java
@@ -0,0 +1,12 @@
+public class Main;
+    public static void main(String[] args){
+        Motorcycle m1 = new Motorcycle( "RED");
+        System.out.println(m1.getColor());
+
+        Motorcycle m2 = new Motorcycle("Blue");
+        System.out.println(m2.getColor())
+
+
+    }
+
+}
--- SCR/Motorcycle.java
+++ SCR/Motorcycle.java
@@ -1,6 +1,14 @@
 public class Motorcycle{
 
-    private String color;
+    private String colorA;
 
+    public Motorcycle(String color1){
+        this.colorA = color1;
+
+
+    }
+
+    public String getColor(){
+    return this.colorA;
 
 }
